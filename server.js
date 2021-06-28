5bfd8389c384f4702437de4bd3552f79b509d1bd
Bahar Hosseini <bahar.hosseini@gmail.com>
2021-06-28 19:31:14 -0400

    production

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express"
+import path from 'path'
+import dirname from 'path'
 import dotenv from 'dotenv'
 import {notFound,errorHandler} from './server/middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -15,6 +17,21 @@ const app = express()
 app.use(express.json());
 
 app.use('/',routePost)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
+
 
 app.use(notFound)
 app.use(errorHandler)
