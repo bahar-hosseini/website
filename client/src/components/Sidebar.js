c9fd5e9ad5320c5500f82639435bb73b0c52c70e
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-10-21 18:30:32 -0400

    update content

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,34 +1,49 @@
-import React from "react"
+import React from 'react'
 
 function Sidebar() {
-
-    return ( 
+  return (
     <div>
-        <section className = "sidebar" >
-        <div className = "text-center">
-        <table style={{backgroundColor:'#FECE2E'}} className = "table" >
-        <tbody>
-        <tr >
-        <th className="px-2 py-4" scope = "col" >
-         <a  href='https://github.com/bahar-hosseini' className = "fab fa-github" target="_blank"rel='noreferrer'></a> 
-        </th>
-        </tr>
-        <tr>
-        <th className = "px-2 py-4" scope = "col">
-        <a href='https://www.linkedin.com/in/maryam-hosseini-163310192/' className = "fab fa-linkedin" target="_blank" rel='noreferrer'> </a>
-        </th>
-        </tr>
-        <tr>
-        <th className = "px-2 py-3" scope = "col">
-        <a href="mailto:bahar.hosseini@gmail.com" className = "fas fa-envelope-square" rel='noreferrer'></a>
-        </th>
-        </tr>
-        </tbody>
-        </table>
+      <section className='sidebar'>
+        <div className='text-center'>
+          <table style={{ backgroundColor: '#FECE2E' }} className='table'>
+            <tbody>
+              <tr>
+                <th className='px-2 py-4' scope='col'>
+                  <a
+                    href='https://github.com/bahar-hosseini'
+                    className='fab fa-github'
+                    target='_blank'
+                    rel='noreferrer'
+                  ></a>
+                </th>
+              </tr>
+              <tr>
+                <th className='px-2 py-4' scope='col'>
+                  <a
+                    href='https://www.linkedin.com/in/bahar-hosseini/'
+                    className='fab fa-linkedin'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    {' '}
+                  </a>
+                </th>
+              </tr>
+              <tr>
+                <th className='px-2 py-3' scope='col'>
+                  <a
+                    href='mailto:bahar.hosseini@gmail.com'
+                    className='fas fa-envelope-square'
+                    rel='noreferrer'
+                  ></a>
+                </th>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        </section>
-        </div>
-    )
+      </section>
+    </div>
+  )
 }
 
-export default Sidebar;
+export default Sidebar
